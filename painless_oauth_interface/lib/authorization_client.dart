5f65106d2844b2c2e28c370cd3fbed73340f42f8
Lukas <luk.meili@bluewin.ch>
2020-04-05 14:06:48 +0200

    Add initial platform interface

---
+++ painless_oauth_interface/lib/authorization_client.dart
@@ -0,0 +1,17 @@
+library painless_oauth_interface;
+
+abstract class AuthorizationClient {
+  /// Url of authorization page
+  /// E.g: (Spotify) https://accounts.spotify.com/authorize
+  /// (Azure)
+  Uri get authorizationUrl;
+  String get responseType;
+  String get clientId;
+  Uri get redirectUri;
+  /// @Nullable. Optional, describes the required access points or features
+  List<String> get scopes;
+  /// @Nullable. Optional but recommended
+  String get state;
+  /// @Nullable. Optional, additional request parameters
+  Map<String, String> get additionalRequestParameters;
+}
--- painless_oauth_interface/lib/painless_oauth_interface.dart
+++ painless_oauth_interface/lib/painless_oauth_interface.dart
@@ -1,7 +1,43 @@
 library painless_oauth_interface;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/cupertino.dart';
+import 'package:painless_oauth_interface/authorization_client.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+/// The interface that implementations of painless_oauth must implement.
+///
+/// If you want to add your own implementation for a specific platform,
+/// use this package as a dependency and extend this class
+abstract class PainlessOAuthPlatform extends PlatformInterface {
+  //this token ensures that the class is extended and not implemented
+  static final Object _token = Object();
+  static PainlessOAuthPlatform _instance;
+
+  static set instance(PainlessOAuthPlatform instance){
+    PlatformInterface.verifyToken(instance, _token);
+    _instance = _instance;
+  }
+
+  static PainlessOAuthPlatform get instance => _instance;
+
+  /// Ensures that an authorization widget is shown. It is not guaranteed that this
+  /// widget is actually the authorization page widget itself. E.g the actual authorization can happen
+  /// in a popup window, in this case this widget should display a loading bar or something similar
+  /// meanwhile the actual action happens on the popup.
+  ///
+  /// [platformSpecificOptions] is meant for individual options on a platform that is only available there.
+  /// This is useful for platform specific options.
+  /// Always start with the platform itself in the key.
+  /// E.g:
+  /// [platformSpecificOptions].putIfAbsent('ios.safariUseJavaScript', () => true);
+  /// [platformSpecificOptions].putIfAbsent('web.useComponent', () => 'iframe');
+  Widget authorizationPage(AuthorizationClient authorizationClient, Map<String, dynamic> platformSpecificOptions){
+    throw UnimplementedError('authorizationPage() has not been implemented.');
+  }
+
+  /// This method returns a map containing the response from the authorization service. Throws an Error when the
+  /// authorization failed.
+  Future<Map<String, dynamic>> listenForResult(){
+    throw UnimplementedError('listenForResult() has not been implemented.');
+  }
 }
