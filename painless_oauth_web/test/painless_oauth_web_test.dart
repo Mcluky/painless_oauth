5ed39d0a9217307e604e3a34bb707e36b2ce556e
Lukas <luk.meili@bluewin.ch>
2020-05-03 22:58:38 +0200

    Fix failing dummy unit test

--- painless_oauth_web/test/painless_oauth_web_test.dart
+++ painless_oauth_web/test/painless_oauth_web_test.dart
@@ -2,11 +2,6 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('dummy test', () {
-    expect(true, false);
-    /*final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);*/
+    expect(true, true);
   });
 }
